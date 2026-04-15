89c285e03132ea1fa5b4e43dbc06b4b29c08f194
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2026-04-15 11:40:47 +0200

    Add dumb page

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,14 @@ main = Blueprint("main", __name__)
 def index():
     return render_template("index.html")
 
+
+@main.route("/classifications")
+def classifications():
+    return render_template("classifications.html")
+
+
+@main.route("/search-by-tag")
+def search_by_tag():
+    return render_template("search_by_tag.html")
+
+
