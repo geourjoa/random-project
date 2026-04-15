b3830932e443770af0528b1a024a6363f71513aa
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2026-04-15 12:56:18 +0200

    Remove secret key useless

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "change-me-in-production")
     DEBUG = os.environ.get("FLASK_DEBUG", "0") == "1"
     DATABASE_PATH = os.environ.get(
         "DATABASE_PATH",
